d2e4554a6a62663489ebbb54a1213422c64344cb
LennartZi <Lennart@zibell.de>
2016-09-24 13:51:48 +0200

    First one working

--- Lego/EV-3/src/ev3/Main.java
+++ Lego/EV-3/src/ev3/Main.java
@@ -1,23 +1,19 @@
 package ev3;
 
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.motor.Motor;
-import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
 public class Main{
 	public static void main(String[] args) {
-		LCD.drawString("Hello World", 1, 1);
+		LCD.drawString("Ist das vegan?", 1, 1);
 		Delay.msDelay(5000);
+		Motor.A.rotate(180);
+		RegulatedMotor m = new EV3MediumRegulatedMotor(MotorPort.A);
+		m.rotate(360);
 	}
 
-
-	public static void motor(String[] args)
-	{
-		RegulatedMotor ma = new NXTRegulatedMotor(MotorPort.A);
-		ma.rotate(360);
-		Motor.A.rotate(-180);
-	}
 }
