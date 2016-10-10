b39f82f4f02b7b7f944996bf7e59d24f7d9bd264
LennartZi <Lennart@zibell.de>
2016-10-10 23:15:16 +0200

    Der Array

--- Lego/EV-3/src/ev3/Main.java
+++ Lego/EV-3/src/ev3/Main.java
@@ -1,18 +1,32 @@
 package ev3;
 
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.motor.EV3MediumRegulatedMotor;
-import lejos.hardware.port.MotorPort;
-import lejos.robotics.RegulatedMotor;
-import lejos.utility.Delay;
+import java.util.Arrays;
 
-public class Main{
-	public static void main(String[] args) {
-		RegulatedMotor m = new EV3MediumRegulatedMotor(MotorPort.A);
-		LCD.drawString("180 | -360", 1, 1);
-		m.rotate(180);
-		Delay.msDelay(5000);
-		m.rotate(-360);
-	}
-
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		String[][] cubeArray = new String[26][3];// Array that has 30(Number of bricks) rows and 3(possible number of colors for each brick) columns
+		{
+			for(int i = 0; i < cubeArray.length; i++)
+			{
+				for(int j = 0; j < cubeArray[i].length; j++)
+				{	
+					cubeArray[i][j] = i + " " + j;	
+				}				
+			}	
+			for(int i = 0; i < cubeArray.length; i++)
+			{
+				for(int j = 0; j < cubeArray[i].length; j++)
+				{	
+					System.out.print("| " + cubeArray[i][j] + " ");	
+				}
+				System.out.println("|");
+			}
+		}
+	}	
+	/*RegulatedMotor p = new EV3MediumRegulatedMotor(MotorPort.A);
+	RegulatedMotor g = new EV3LargeRegulatedMotor(MotorPort.B);
+	p.rotate(180);
+	*/
 }
