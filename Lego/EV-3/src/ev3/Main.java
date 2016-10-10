23c2054448fecd1a716592fa6124cece985c27f2
LennartZi <Lennart@zibell.de>
2016-10-10 23:17:58 +0200

    Die Comments

--- Lego/EV-3/src/ev3/Main.java
+++ Lego/EV-3/src/ev3/Main.java
@@ -1,21 +1,22 @@
 package ev3;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main 
 {
 	public static void main(String[] args)
 	{
-		String[][] cubeArray = new String[26][3];// Array that has 30(Number of bricks) rows and 3(possible number of colors for each brick) columns
+		String[][] cubeArray = new String[26][3];// Array 26(steine)*3(mögliche Farben)
 		{
-			for(int i = 0; i < cubeArray.length; i++)
+			for(int i = 0; i < cubeArray.length; i++)//Den Array mit zahlen füllen
 			{
 				for(int j = 0; j < cubeArray[i].length; j++)
 				{	
 					cubeArray[i][j] = i + " " + j;	
 				}				
 			}	
-			for(int i = 0; i < cubeArray.length; i++)
+			for(int i = 0; i < cubeArray.length; i++)//Den Array als "Tabelle" ausgeben
 			{
 				for(int j = 0; j < cubeArray[i].length; j++)
 				{	
