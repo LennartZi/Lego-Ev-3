c71b4f9fba7c024d48c6f9ef5fd15662eee1c384
LennartZi <Lennart@zibell.de>
2016-10-28 23:39:02 +0200

    small one includes the scan method and tests

--- Lego/EV-3/src/ev3/Main.java
+++ Lego/EV-3/src/ev3/Main.java
@@ -1,33 +1,80 @@
 package ev3;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
 
 public class Main 
 {
+	RegulatedMotor p = new EV3MediumRegulatedMotor(MotorPort.A);
+	RegulatedMotor g = new EV3LargeRegulatedMotor(MotorPort.B);
 	public static void main(String[] args)
 	{
 		String[][] cubeArray = new String[26][3];// Array 26(steine)*3(mögliche Farben)
+		String[][] tauschArr = new String[26][3];// Array zum drehen
 		{
-			for(int i = 0; i < cubeArray.length; i++)//Den Array mit zahlen füllen
+			for(int i = 0; i < cubeArray.length; i++)//Den Array mit farben(colour) füllen
 			{
 				for(int j = 0; j < cubeArray[i].length; j++)
-				{	
-					cubeArray[i][j] = i + " " + j;	
-				}				
-			}	
-			for(int i = 0; i < cubeArray.length; i++)//Den Array als "Tabelle" ausgeben
-			{
-				for(int j = 0; j < cubeArray[i].length; j++)
-				{	
-					System.out.print("| " + cubeArray[i][j] + " ");	
+				{
+					String colour = scan();
+					cubeArray[i][j] = colour;
+					LCD.drawString(colour, 1, 1);
 				}
-				System.out.println("|");
+			}
+			for(int i = 0; i < cubeArray.length; i++)
+			{
+				for(int j = 0; j < cubeArray[i].length; j++)
+				{	
+					tauschArr[i][j] = cubeArray [i][j];
+				}
 			}
 		}
-	}	
-	/*RegulatedMotor p = new EV3MediumRegulatedMotor(MotorPort.A);
-	RegulatedMotor g = new EV3LargeRegulatedMotor(MotorPort.B);
-	p.rotate(180);
-	*/
+	}
+	public static String scan()//scant.....
+	{
+		Port sensorPort = LocalEV3.get().getPort("S1");            
+		EV3ColorSensor colorSensor = new EV3ColorSensor(sensorPort);
+		SampleProvider colorRGBSensor = colorSensor.getRGBMode();
+		int sampleSize = colorRGBSensor.sampleSize();            
+		float[] sample = new float[sampleSize];
+		
+		String colour;
+		colorSensor.fetchSample(sample, 0);
+		
+		if(sample[0]<0.14 && sample[0]>0.2 && sample[1]<0.14 && sample[1]>0.2 && sample[2]<0.14 && sample[2]>0.2){ //gleicht die drei farbwerte ab
+			colour = "white";
+			return colour;
+		}
+		else if(sample[0]<0.14 && sample[0]>0.2 && sample[1]<0.14 && sample[1]>0.2 && sample[2]<0.14 && sample[2]>0.2){
+			colour = "red";
+			return colour;
+		}
+		else if(sample[0]<0.14 && sample[0]>0.2 && sample[1]<0.14 && sample[1]>0.2 && sample[2]<0.14 && sample[2]>0.2){
+			colour = "green";
+			return colour;
+		}
+		else if(sample[0]<0.14 && sample[0]>0.2 && sample[1]<0.14 && sample[1]>0.2 && sample[2]<0.14 && sample[2]>0.2){
+			colour = "orange";
+			return colour;
+		}
+		else if(sample[0]<0.14 && sample[0]>0.2 && sample[1]<0.14 && sample[1]>0.2 && sample[2]<0.14 && sample[2]>0.2){
+			colour = "blue";
+			return colour;
+		}
+		else if(sample[0]<0.14 && sample[0]>0.2 && sample[1]<0.14 && sample[1]>0.2 && sample[2]<0.14 && sample[2]>0.2){
+			colour = "yellow";
+			return colour;
+		}
+		else{
+			System.exit(0);
+			return "error";
+		}
+	}
 }
